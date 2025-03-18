1fa111724907c1c8744c0634926e41e3948c177e
PabloEncinasLozano <pel1001@alu.ubu.es>
2025-03-18 16:19:12 +0100

    Primer Commit

---
+++ src/main/java/SisDist/SistDistPFinal/SistDistPFinalApplication.java
@@ -0,0 +1,13 @@
+package SisDist.SistDistPFinal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistDistPFinalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistDistPFinalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/SisDist/SistDistPFinal/SistDistPFinalApplicationTests.java
@@ -0,0 +1,13 @@
+package SisDist.SistDistPFinal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistDistPFinalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
